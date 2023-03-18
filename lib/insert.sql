0299d705ccc79250a0d3d04304dbc776b4e66f14
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-17 18:45:21 -0700

    updated

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 12, "M", "Brown", "Calm", true),
+("Rowdy", 4, "M", "Brown", "rousy", true),
+("Tabitha", 15, "F", "Brown", "Calm", false),
+("Sergeant Brown", 18, "M", "Black", "Calm", true),
+("Melissa", 8, "F", "Black", "Calm", true),
+("Grinch", 4, "M", "Brown", "Aggresive", true),
+("Wendy", 7, "F", "Black", "Calm", true),
+(NULL, 7, "F", "Black", "Calm", true);
